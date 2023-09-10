59301c708e940c27467d4da87d884b91bdc86ec4
holmanej <holmanej@gmail.com>
2023-09-10 15:08:08 -0700

    Added opentk_window class

---
+++ GuildLeader/Program.cs
@@ -0,0 +1,26 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace GuildLeader
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new Vector2i(1600, 900),
+                Title = "Guild Leader",
+                // This is needed to run on macos
+                Flags = ContextFlags.ForwardCompatible,
+            };
+
+            // To create a new window, create a class that extends GameWindow, then call Run() on it.
+            using (var window = new Window(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                window.Run();
+            }
+        }
+    }
+}
